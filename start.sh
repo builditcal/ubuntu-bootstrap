ec9a6160d4cb1b9095cc1f4db5f0c7086ad5565f
Callum <38749032+builditcal@users.noreply.github.com>
2025-03-01 18:42:28 +0000

    small updates

--- start.sh
+++ start.sh
@@ -27,7 +27,7 @@ for try in $(seq 1 $MAX_TRIES); do
   snap list 2> /dev/null | grep -v ^Name |  awk '{ print $1 }'  | xargs -r -n1  sudo snap remove || true
 done
 
-sudo apt autoremove --purge snapd
+sudo apt autoremove -yq --purge snapd
 sudo apt-mark hold snapd
 sudo rm -rf /snap
 sudo rm -rf $HOME/snap
@@ -44,7 +44,7 @@ echo "*****************************************************"
 mkdir $DOWNLOAD_PATH
 # VS CODE
 echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
-sudo apt-get install wget gpg
+sudo apt-get install -yq wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
