3bc52dfcdd61a5f156b1eb80423041802c9080d3
Callum <38749032+builditcal@users.noreply.github.com>
2025-03-01 18:58:10 +0000

    update before code install

--- start.sh
+++ start.sh
@@ -47,9 +47,10 @@ echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
 sudo apt-get install -yq wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
 rm -f packages.microsoft.gpg
 sudo apt install -yq apt-transport-https
+sudo apt update
 sudo apt install -yq code
 
 # Chrome
@@ -86,4 +87,4 @@ sudo apt -yq install gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # bitwarden
-flatpak install -y flathub com.bitwarden.desktop
+sudo flatpak install -y flathub com.bitwarden.desktop
